4348a5be84928a548cb2819c1f631c93000e4c41
Arfaoui Med Amine <mohamedamine.arfaoui@esprit.tn>
2020-09-16 14:11:21 +0100

    initial commit for the sake of yahia hajri

---
+++ src/main/java/tn/JCITAZ/spring/JciApplication.java
@@ -0,0 +1,13 @@
+package tn.JCITAZ.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/JCITAZ/spring/JciApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.JCITAZ.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
